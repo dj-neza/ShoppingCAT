337bf74d75d66f508bd5bb2c4014f491872306ae
Neza Dukic <neza.djukic@gmail.com>
2017-09-16 14:49:37 +0200

    extension done & polished

--- extension/screenshot.js
+++ extension/screenshot.js
@@ -1,6 +1,8 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+/**
+ *  @author ShoppingCAT
+ *  2017
+ */
+
 var cropper;
 function setScreenshotUrl(url) {
   document.getElementById('target').src = url;
