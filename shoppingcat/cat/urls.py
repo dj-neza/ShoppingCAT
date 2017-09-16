f826b754f8fb5ad8b5c7ec1864fb6b164f9b8f04
Anja <Anja>
2017-09-16 06:02:43 +0200

    dodan logout

--- shoppingcat/cat/urls.py
+++ shoppingcat/cat/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
 	url(r"^$", views.index, name="index"),
-	url(r"^home/$", views.home, name="home")
+	url(r"^home/$", views.home, name="home"),
+	url(r'^logout/', views.logout, name='logout'), 
 ]
--- shoppingcat/cat/views.py
+++ shoppingcat/cat/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_protect
+from django.contrib.auth import authenticate, login, logout
 
 from .forms import *
 
@@ -16,4 +17,20 @@ def home(request):
 def index(request):
 	form = SignIn(request.POST or None)
 
+	if form.is_valid():
+		data = form.cleaned_data
+		username = data["email"]
+		password = data["password"]
+		user = authenticate(username=username, password=password)
+		login(request, user)
+
 	return render(request, 'home.html', {'form': form})
+
+# log out
+def logout(request):
+	if request.user.is_authenticated:
+		logout(request)
+
+	form = SignIn(request.POST or None)
+
+	return render(request, 'home.html', {'form':form})
