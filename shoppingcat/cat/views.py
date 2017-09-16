d8edcfb700f3f1e94545bc7fdfeb3acce164ccaa
Anja <Anja>
2017-09-16 02:43:08 +0200

    basic page

--- shoppingcat/cat/views.py
+++ shoppingcat/cat/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+	if request.user.is_authenticated():
+		return render(request, 'index.html')
+
+	return render(request, 'home.html')
+
+def index(request):
+	return render(request, 'home.html')
